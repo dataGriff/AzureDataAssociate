46f571929224f8a9371556bab38cfdfafc58cd9f
Richard Griffiths <griff182uk@yahoo.co.uk>
2019-10-05 17:58:06 +0100

    got site splitting working

---
+++ StreamTesting/StreamTesting/Script.asaql.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace StreamTesting
+{
+    public class Class1
+    {
+        
+    }
+}
